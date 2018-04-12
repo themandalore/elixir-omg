303bd1d0949819693c2ebe6f423235ce3cd23343
Paweł <inspiredswiftness@gmail.com>
2018-04-12 14:02:33 +0200

    Refactor Depositor.Core to EventListener.Core

--- apps/omisego_api/lib/depositor.ex
+++ apps/omisego_api/lib/depositor.ex
@@ -5,7 +5,7 @@ defmodule OmiseGO.API.Depositor do
   """
 
   alias OmiseGO.Eth
-  alias OmiseGO.API.Depositor.Core
+  alias OmiseGO.API.EventListener.Core
   alias OmiseGO.API.State
 
   ### Client
@@ -20,22 +20,33 @@ defmodule OmiseGO.API.Depositor do
 
   def init(:ok) do
     #TODO: initialize state with the last ethereum block we have seen deposits from
+    block_finality_margin = Application.get_env(:omisego_api, :depositor_block_finality_margin)
+    max_blocks_in_fetch = Application.get_env(:omisego_api, :depositor_max_block_range_in_deposits_query)
+    get_deposits_interval = Application.get_env(:omisego_api, :depositor_get_deposits_interval_ms)
+
     with {:ok, parent_start} <- Eth.get_root_deployment_height() do
       schedule_get_deposits(0)
-      {:ok, %Core{last_deposit_block: parent_start}}
+      {:ok,
+       %Core{
+         last_event_block: parent_start,
+         block_finality_margin: block_finality_margin,
+         max_blocks_in_fetch: max_blocks_in_fetch,
+         get_events_inerval: get_deposits_interval
+       }
+      }
     end
   end
 
   def handle_info(:get_deposits, state) do
     with {:ok, eth_block_height} <- Eth.get_ethereum_height(),
          {:ok, new_state, next_get_deposits_interval, eth_block_from, eth_block_to} <-
-           Core.get_deposit_block_range(state, eth_block_height),
+           Core.get_events_block_range(state, eth_block_height),
          {:ok, deposits} <- Eth.get_deposits(eth_block_from, eth_block_to),
          :ok <- State.deposit(deposits) do
       schedule_get_deposits(next_get_deposits_interval)
       {:no_reply, new_state}
     else
-      {:no_blocks_with_deposit, state, next_get_deposits_interval} ->
+      {:no_blocks_with_event, state, next_get_deposits_interval} ->
         schedule_get_deposits(next_get_deposits_interval)
         {:no_reply, state}
       _ -> {:stop, :failed_to_get_deposits, state}
