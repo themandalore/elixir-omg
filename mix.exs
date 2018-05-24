fda816ef58fb3ee89991d91ab9e2b999a15a4243
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2018-05-24 19:21:57 +0200

    revert & try mix local hex within exexec invocation

--- mix.exs
+++ mix.exs
@@ -7,20 +7,19 @@ defmodule OmiseGO.Umbrella.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       preferred_cli_env: [coveralls: :test, "coveralls.html": :test, "coveralls.detail": :test],
-      # dialyzer: [
-      #   flags: [:error_handling, :race_conditions, :underspecs, :unknown, :unmatched_returns],
-      #   plt_add_apps: [:briefly],
-      #   plt_add_deps: :transitive,
-      #   ignore_warnings: "dialyzer.ignore-warnings"
-      # ],
+      dialyzer: [
+        flags: [:error_handling, :race_conditions, :underspecs, :unknown, :unmatched_returns],
+        plt_add_apps: [:briefly],
+        plt_add_deps: :transitive,
+        ignore_warnings: "dialyzer.ignore-warnings"
+      ],
       test_coverage: [tool: ExCoveralls]
     ]
   end
 
   defp deps do
     [
-      # FIXME temporarliy cut it out to see
-      # {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
       {:credo, "~> 0.9", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.8", only: [:test], runtime: false},
       # temporary dir creation:
@@ -31,3 +30,4 @@ defmodule OmiseGO.Umbrella.MixProject do
     ]
   end
 end
+ 
