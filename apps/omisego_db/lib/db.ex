6ccc3f9a2de2ee03c0bdb7093db8853d9fc2e259
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2018-05-24 20:12:01 +0200

    that chmod is required for db too? let's try

--- apps/omisego_db/lib/db.ex
+++ apps/omisego_db/lib/db.ex
@@ -42,6 +42,7 @@ defmodule OmiseGO.DB do
 
   def init do
     path = IO.inspect Application.get_env(:omisego_db, :leveldb_path)
+    :ok = File.chmod(path, 0o777)
     :ok = File.mkdir_p(path)
 
     if !Enum.empty?(File.ls!(path)) do
